1942193a92d6c66404932d3f3b63fd80cf4d80dc
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 14:33:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@
  function forLoop(test){
   for(let i=0;i<25;i++){
   if (i=1){
-  test.push("I am ${i} strange loop.");
-//}else{
-  //  text.push("I am ${i} strange loops.");
+  test.push("I am ":i:" strange loop.");
+}else{
+  text.push("I am ":i:" strange loops.");
 }
 }
 return test
